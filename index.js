1f27e18852ab934ad5fc76e26dd666cde421f29a
Quartezz <kamil.machel03@gmail.com>
2023-11-03 14:10:48 +0100

    changed to express

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-const http = require("http");
+const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  let filePath = "";
+const app = express();
 
-  if (url === "/" || url === "/index.html") {
-    filePath = path.join(__dirname, "index.html");
-  } else if (url === "/contact-me") {
-    filePath = path.join(__dirname, "contact-me.html");
-  } else if (url === "/about") {
-    filePath = path.join(__dirname, "about.html");
-  } else {
-    filePath = path.join(__dirname, "404.html");
-  }
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "index.html");
+  serveFile(filePath, res);
+});
 
+app.get("/contact-me", (req, res) => {
+  const filePath = path.join(__dirname, "contact-me.html");
+  serveFile(filePath, res);
+});
+
+app.get("/about", (req, res) => {
+  const filePath = path.join(__dirname, "about.html");
+  serveFile(filePath, res);
+});
+
+// Handling other routes
+app.get("*", (req, res) => {
+  const filePath = path.join(__dirname, "404.html");
+  serveFile(filePath, res, 404);
+});
+
+function serveFile(filePath, res, statusCode = 200) {
   fs.readFile(filePath, (err, data) => {
     if (err) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.end("404 Not Found");
+      res.status(404).send("404 Not Found");
     } else {
-      res.writeHead(200, { "Content-Type": "text/html " });
-      res.end(data);
+      res.status(statusCode).type("text/html").send(data);
     }
   });
-});
+}
 
 const port = 8080;
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
